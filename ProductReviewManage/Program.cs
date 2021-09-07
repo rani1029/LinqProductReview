dc7168aeb55ce20b9140528a6e4d23505f57e4ec
rani1029 <ranig1029@gmail.com>
2021-09-08 00:36:25 +0530

    welcome msg added

---
+++ ProductReviewManage/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductReviewManage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WElcome to product review management");
+        }
+    }
+}
